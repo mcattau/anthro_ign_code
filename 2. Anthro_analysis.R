f63aa37ba8e481881097ca9fc3ff8be80e0b4461
MCattau <megan.cattau@gmail.com>
2019-12-01 09:09:44 -0700

    add A to author

--- 2. Anthro_analysis.R
+++ 2. Anthro_analysis.R
@@ -2,7 +2,7 @@
 
 # Anthropogenic and lightning-started fires are becoming larger and more frequent over a longer season length in the U.S.
 # Global Ecology and Biogeography
-# Code author: Megan Cattau
+# Code author: Megan Cattau and Adam Mahood
 # Contact info: megan.cattau@gmail.com or megancattau@boisestate.edu
 # Manuscript authors: Megan Cattau, Carol Wessman, Adam Mahood, Jennifer Balch
 # Project description: This project aims to elucidate how fire physical characteristics are changing over time in the US, the influence of anthropogenic ignitions on fire physical characteristics, and how these patterns vary over space and time.
